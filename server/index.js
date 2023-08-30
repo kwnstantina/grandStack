8f62c4e41696d8060f9a12c7f7f69bfbf4e121b3
konstantina kirtsia <kwnstantinakirtsia1996@gmail.com>
2023-08-30 18:26:55 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+import { movieSchema } from './grapqlSchema';
+
+dotenv.config();
+
+const typeDefs = movieSchema;
+
+const driver = neo4j.driver(
+    "bolt://localhost:7687",
+    neo4j.auth.basic("neo4j", "password")
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+const server = new ApolloServer({
+    schema: await neoSchema.getSchema(),
+});
+
+const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => ({ req }),
+    listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at ${url}`);
